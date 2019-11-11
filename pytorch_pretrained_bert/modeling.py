8f44ffb9cfcb1f929b4f9b3fb5895a2e023b8d3f
hanshiyi <hanshiyi123@outlook.com>
2019-11-11 17:42:44 -0500

    updated pos qa bert model

--- pytorch_pretrained_bert/modeling.py
+++ pytorch_pretrained_bert/modeling.py
@@ -1045,6 +1045,44 @@ class BertForQuestionAnswering(PreTrainedBertModel):
         else:
             return start_logits, end_logits
 
+class BertPOSQuestionAnswering(PreTrainedBertModel):
+
+    def __init__(self, config, posconfig):
+        super(BertPOSQuestionAnswering, self).__init__(config)
+        self.bert = BertModel(config)
+        self.posbert = BertModel(posconfig)
+        # TODO check with Google if it's normal there is no dropout on the token classifier of SQuAD in the TF version
+        # self.dropout = nn.Dropout(config.hidden_dropout_prob)
+        self.qa_outputs = nn.Linear(config.hidden_size+posconfig.hidden_size, 2)
+        self.apply(self.init_bert_weights)
+
+    def forward(self, input_ids, pos_ids, token_type_ids=None, attention_mask=None, start_positions=None, end_positions=None):
+        sequence_output, _ = self.bert(input_ids, token_type_ids, attention_mask, output_all_encoded_layers=False)
+        pos_output, _ = self.posbert(pos_ids, token_type_ids, attention_mask, output_all_encoded_layers=False)
+        logits = self.qa_outputs(torch.cat([sequence_output, pos_output], dim=-1))
+        start_logits, end_logits = logits.split(1, dim=-1)
+        start_logits = start_logits.squeeze(-1)
+        end_logits = end_logits.squeeze(-1)
+
+        if start_positions is not None and end_positions is not None:
+            # If we are on multi-GPU, split add a dimension
+            if len(start_positions.size()) > 1:
+                start_positions = start_positions.squeeze(-1)
+            if len(end_positions.size()) > 1:
+                end_positions = end_positions.squeeze(-1)
+            # sometimes the start/end positions are outside our model inputs, we ignore these terms
+            ignored_index = start_logits.size(1)
+            start_positions.clamp_(0, ignored_index)
+            end_positions.clamp_(0, ignored_index)
+
+            loss_fct = CrossEntropyLoss(ignore_index=ignored_index)
+            start_loss = loss_fct(start_logits, start_positions)
+            end_loss = loss_fct(end_logits, end_positions)
+            total_loss = (start_loss + end_loss) / 2
+            return total_loss
+        else:
+            return start_logits, end_logits
+
 
 if __name__ == "__main__":
     config = BertConfig(200, hidden_size=20,
